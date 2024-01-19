3fe546006635be8ac10bd03cda354aa4eaad0c94
Geovanna Otoni <geovannaotoni@gmail.com>
2024-01-19 15:29:53 -0300

    feat: includes router and error handler to app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import connectToMongoDB from './db/connection';
+import router from './routes';
+import ErrorHandler from './middlewares/ErrorHandler';
 
 const app = express();
 
@@ -8,6 +10,9 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.use(router);
+app.use(ErrorHandler.handle);
 
 const PORT = process.env.PORT || 3000;
 
