92cdce355d5eadc564d3700dc1eb7bcddd6b5bb7
Geovanna Otoni <geovannaotoni@gmail.com>
2024-01-19 15:21:36 -0300

    chore: initial configuration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import connectToMongoDB from './db/connection';
 
 const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 const PORT = process.env.PORT || 3000;
+
+connectToMongoDB();
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
